9244c4eed76daa57e56c798ec12e681e18f75c71
mprat <michele.pratusevich@gmail.com>
2012-12-18 18:17:38 -0500

    numplayers command line argument

--- python/main.py
+++ python/main.py
@@ -3,10 +3,11 @@ import sys
 from game import *
 
 if __name__ == '__main__':
-	if (len(sys.argv) != 2):
+	if (len(sys.argv) != 3):
 		sys.exit(0)
 	else:
 		deck = Deck(int(sys.argv[1]))
-		game = Game(deck, 3)
+		numplayers = int(sys.argv[2])
+		game = Game(deck, numplayers)
 		game.startGame()
 		

b03f858b480ed30acb726857c2565da722bb7eac
mprat <michele.pratusevich@gmail.com>
2012-12-26 20:12:50 -0500

    comments added to dealer player

--- python/dealerPlayer.py
+++ python/dealerPlayer.py
@@ -3,6 +3,7 @@ import player as player
 
 class DealerPlayer(player.Player):
 	#TODO: implement turn
+	#dealer hits until 17 (some casinos have dealer hit on soft 17, most don't)
 	def takeTurn(self):
 		handval = deck.getHandVal(self.hand)
 		print "handval = ", handval

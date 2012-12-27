8a8073e096858ad816c73e1bc0f897d126e35ad0
mprat <michele.pratusevich@gmail.com>
2012-12-26 22:38:51 -0500

    default is only two players: dealer and 1 human player

--- python/main.py
+++ python/main.py
@@ -3,10 +3,11 @@ import sys
 from game import *
 
 if __name__ == '__main__':
-	if (len(sys.argv) != 2):
-		sys.exit(0)
-	else:
-		deck = Deck(int(sys.argv[1]))
-		game = Game(deck)
-		game.startGame()
+#	if (len(sys.argv) != 2):
+#		sys.exit(0)
+#	else:
+#		deck = Deck(int(sys.argv[1]))
+	deck = Deck(1)
+	game = Game(deck)
+	game.startGame()
 		
